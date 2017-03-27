391723b71245a3e88460959a09a087ff6997b493
garthbeetle <robasher9@Hotmail.com>
2017-03-27 14:37:40 +1100

    added layer filter and resolution

--- viewExporter.py
+++ viewExporter.py
@@ -1,18 +1,47 @@
 import rhinoscriptsyntax as rs
+import Rhino
+
+doc = Rhino.RhinoDoc.ActiveDoc
+activeViewport = doc.Views.ActiveView.ActiveViewport
+viewport_info = Rhino.DocObjects.ViewportInfo(activeViewport)
+
+
+folder = rs.BrowseForFolder(message="Browse for folder", title="Save Frames")
+if folder is None: quit()
+
+option = rs.GetString("Option Name")
+scale = rs.GetString("resolution?")
+
+
+width =  activeViewport.Size.Width*int(scale)
+height =  activeViewport.Size.Height*int(scale)
+
 
 
 eType = rs.GetString(message = "Input the view filter!")
+vType = rs.GetString(message = "Input the layer filter!")
 eViews = []
 eLayerStates = []
-fileFolder = r'C:\Users\s-rasher\Desktop\coxoption'
+fileFolder = str(folder)
 plugin = rs.GetPlugInObject("Rhino Bonus Tools")
 if plugin is not None:
     eViews = rs.NamedViews()
     eLayerStates = plugin.LayerStateNames()
     for i in range(len(eViews)):
+        rs.RestoreNamedView(eViews[i])
         if eType in eViews[i]:
-            rs.RestoreNamedView(eViews[i])
+            print 'yah'
             for j in range(len(eLayerStates)):
-                plugin.RestoreLayerState(eLayerStates[j])
-                imgDrop = '"' + fileFolder + str(eType) + str(i) + str(j) + '.png"'
-                rs.Command("-_ViewCapturetoFile " + imgDrop + " Width=2000 Height=1500 Scale=1 DrawGrid=No DrawWorldAxes=No DrawCPlaneAxes=No TransparentBackground=No" + " _Enter")
+                print j
+                view = str(eViews[i])
+                if vType in eLayerStates[j]:
+                    plugin.RestoreLayerState(eLayerStates[j])
+                    rs.RestoreNamedView(eViews[i])
+                    circle = rs.AddCircle([0,0,0], 2)
+                    rs.DeleteObject(circle)
+                    name = str(eLayerStates[j]) + option + view
+                    command = "-ViewCapturetoFile \"" + fileFolder + "\\" + name + ".png\" "
+                    sizeCommand = " Width=" + str(width) + " Height=" + str(height)
+                    #rs.Command(" Width=1180 Height=738 Scale=1 DrawGrid=No DrawWorldAxes=No DrawCPlaneAxes=No TransparentBackground=Yes")
+                    rs.Command(command + sizeCommand + " _Enter")
+                print j
